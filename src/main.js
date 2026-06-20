6c4e142b3b7be66db02bf8ccdbf6977677e458c6
Moishe Lettvin <moishel@gmail.com>
2026-06-20 17:18:24 -0600

    Remove temporary build-check scaffolding

--- src/main.js
+++
@@ -1,6 +0,0 @@
-// IR Converter main entry point
-// The createRenderer is imported by the app at runtime
-export { createRenderer } from './webgl.js';
-export { transform } from './transform.js';
-export { loadPresets, savePreset, deletePreset } from './presets.js';
-export { DEFAULTS } from './defaults.js';
